daeb1bc09823db8dbf2af2817c2b0ad62916748b
Cruz Rocha <juancruzrocha@live.com>
2023-02-03 02:03:16 -0300

    Update SearchBar.jsx

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -3,17 +3,27 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const SearchBar = (props) => {
+  // estado local del input de busqueda
   const [busqueda, setBusqueda] = useState("");
+  //estado global que tiene a todos los perros []
   const state = useSelector((state) => state.dogs);
-
-  const filtrar = (terminoBusqueda) => {
-    state.filter((dog) => {if(dog.name === terminoBusqueda) return dog});
-  };
 
   const handleChange = (e) => {
     setBusqueda(e.target.value);
-    filtrar(busqueda);
   };
+
+
+  const searchResult = (terminoBusqueda) => {
+    let result = state.filter((dog) => dog.name === terminoBusqueda);
+    return result;
+  };
+
+  console.log("soy el global-state.dogs:", state);
+  console.log("soy el local-state-busqueda:", busqueda);
+  console.log(
+    "soy el resultado de la ejecucion de filtrar(busqueda)",
+    searchResult(busqueda)
+  );
 
   return (
     <>
@@ -24,6 +34,7 @@ const SearchBar = (props) => {
           type="text"
           placeholder="Search by breed"
           onChange={handleChange}
+          
         />
         <input type="submit" value="Search" />
       </form>
